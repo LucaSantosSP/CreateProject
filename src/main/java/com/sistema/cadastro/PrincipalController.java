660a72e6e17f8a59453294afcc46b4857406eb89
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-07-06 18:19:19 -0300

    Criado a página de cadastro

--- src/main/java/com/sistema/cadastro/PrincipalController.java
+++ src/main/java/com/sistema/cadastro/PrincipalController.java
@@ -1,11 +1,18 @@
 package com.sistema.cadastro;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PrincipalController {
 	
 	@GetMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/cadastro")
+	public String cadastro() {
+		return "cadastro";
 	}
 }
